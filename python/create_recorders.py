3b370928a5261b3c7fdc3bb055c6b85effc38d82
MidrarAdham <midrar@pdx.edu>
2023-03-07 17:50:37 -0800

    created_recorders

--- python/create_recorders.py
+++ python/create_recorders.py
@@ -24,35 +24,26 @@ def convert_glm_json(glm_path, downstream_obj, json_path):
 def setup_recorder_names(json_path, downstream_obj, glm_path):
     counter = 1
     multi_recorders = open(f'{glm_path}multi_recorders.glm','w')
-    reco = ''
+
     with open(f"{json_path}{downstream_obj}.json", "r") as file:
         data = json.load(file)
     
     index = [i for i in range(960)]
     power_prop = ["measured_real_power" for i in range(960)]
     meter_names = []
+    recorders_property = []
     rec = ''
     for i in data['objects']:
         if 'name' in i['attributes']:
             if i['attributes']['name'].startswith('trip_node_meter'):
                 meter_names.append(i['attributes']['name'])
     for i, names, pwr_prop in zip(index, meter_names, power_prop):
-        if i > 0:
-            rec += ','
-        
-        rec += f'{names}:{pwr_prop}'
-    print(rec)
-
-    # for i, x in enumerate(meter_names):
-    #     if i > 0:
-    #         reco += ','
-    #     reco += f'{x}:{power_prop[i]}'
-    #     if i > 10:
-    #         break
-    # print(type(reco))
-
-
-        
+        rec = f'{names}:{pwr_prop}'
+        recorders_property.append(rec)
+    
+    for i in range(0, len(recorders_property), 25):
+        group = recorders_property[i: i+25]
+        print(f"object multi_recorder {{\n\tinterval 60;\n\tproperty {' '.join(str(meters) for meters in group)};\n\tfile ./{glm_path}glm_output/meter_{counter}.csv;\n}}", file=multi_recorders)
 
 
 def main(json_path, glm_path, up_stream_objects, downstream_obj):
--- python/wd_profiles_extorsion.py
+++ python/wd_profiles_extorsion.py
@@ -1,40 +1,69 @@
 import os
+import numpy as np
 import pandas as pd
 
-wd_pop = "/home/deras/Desktop/Midrar_work/thesis_work/feeder_model/basecase/final_files/"
 
-def export_wd_files(wd_pop):
-    counter = 1
-    for files in os.listdir(wd_pop):
-        if files.startswith("wd_"):
-            df = pd.read_csv(os.path.join(wd_pop, files), names=['time','gpm'], parse_dates=True)
-            df['time'] = pd.to_datetime(df['time'])
-            df.set_index('time', inplace=True)
-            for day in df.index.day.unique():
-                day_df = df[df.index.day == day]
-                new_range = pd.date_range(start=f"2021-12-25 00:00:00", end=f"2021-12-25 23:59:00", freq='1T')
-                print('len old --> ',len(day_df.index), 'len new --> ', len(new_range), 'issue here -->',files)
-                if len(day_df.index) == len(new_range):
-                    day_df.index = new_range
-                    day_df.to_csv(f"../wd_files/wd_{counter}.csv", header=False)
-                    counter += 1
-                else:
-                    pass
-
-    # counter = 1
-    # for files in os.listdir(wd_pop):
-    #     if files.startswith("wd_"):
-    #         df = pd.read_csv(wd_pop+files, names=['time','gpm'], parse_dates=True)
-    #         df['time'] = pd.to_datetime(df['time'])
-    #         df.set_index('time', inplace=True)
-    #         for day in df.index.day.unique():
-    #             day_df = df[df.index.day == day]
-    #             new_range = pd.date_range(start=f"2021-12-25 00:00:00", end=f"2021-12-25 23:59:00", freq='1T')
-    #             day_df.index = new_range
-    #             day_df.to_csv(f"../wd_files/wd_{counter}.csv")
-    #             counter +=1
+wd_files = "/home/deras/Desktop/midrar_work_github/dhw-generator/outputs/"
 
 
-def main(wd_pop):
-    export_wd_files(wd_pop)
-main(wd_pop)
+def export_wd_files(wd_files, file_name):
+    df = pd.read_csv(wd_files+file_name)
+    df["timestamp"] = pd.to_timedelta(pd.to_datetime(df["end_time"]) - pd.to_datetime(df["start_time"]))
+    
+    new_data = {'timestamp':[], 'draw':[]}
+    new_df = pd.DataFrame(new_data)
+
+    elapsed_time = pd.Timedelta(0)
+    sum_draw = 0
+
+    for i, row in df.iterrows():
+        elapsed_time += row['timestamp']
+        sum_draw += row['draw']
+
+        if elapsed_time >= pd.Timedelta('00:01:00'):
+            new_row = {'timestamp':pd.Timedelta('00:01:00'), 'draw':sum_draw}
+            new_df = new_df.append(new_row, ignore_index=True)
+            elapsed_time = elapsed_time - pd.Timedelta('00:01:00')
+            sum_draw = 0
+    new_df['timestamp'] = df['start_time']
+    
+    return new_df
+
+def create_full_day_df(df):
+
+    df['timestamp'] = pd.to_datetime(df['timestamp']).dt.floor('min')
+
+    data = {'timestamp':pd.date_range(start='2021-12-25 00:00:00', end='2021-12-25 23:59:00', freq='1T'),
+            'draw':np.zeros(1440)}
+    
+    full_day_df = pd.DataFrame(data)
+
+    merged_dfs = pd.merge(full_day_df, df, on='timestamp', how='left')
+    
+    merged_dfs['draw_x'] = merged_dfs['draw_x'].fillna(merged_dfs['draw_y'])
+
+    merged_dfs = merged_dfs.drop('draw_x', axis=1)
+
+    merged_dfs = merged_dfs.rename(columns={'draw_y':'draw'})
+
+    merged_dfs['draw'] = (merged_dfs['draw'].fillna(0)).round(3)
+
+    return merged_dfs
+
+def wr_csv(df, file_counter):
+    df.to_csv(f'wd_{file_counter}.csv')
+    file_counter += 1
+
+def main(wd_files):
+
+    file_counter = 1
+
+    for file_name in os.listdir(wd_files):
+
+        df = export_wd_files(wd_files, file_name)
+
+        df = create_full_day_df(df)
+
+        wr_csv(df, file_counter)
+
+main(wd_files)
